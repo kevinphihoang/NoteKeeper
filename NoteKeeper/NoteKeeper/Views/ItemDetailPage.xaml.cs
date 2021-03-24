eb8a2744e99b75c7fbfd14e52ec65f095921eb6d
kevinphihoang <kevinphi_hoang@tjx.com>
2021-03-24 12:07:38 -0400

    Starting point for Pluralsight.

---
+++ NoteKeeper/NoteKeeper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NoteKeeper.Models;
+using NoteKeeper.ViewModels;
+
+namespace NoteKeeper.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+
+        public void Cancel_Clicked(object sender, EventArgs eventArgs)
+        {
+            DisplayAlert("Cancel option", "Cancel was selected", "Button 2", "Button 1");
+        }
+
+        public void Save_Clicked(object sender, EventArgs eventArgs)
+        {
+            DisplayAlert("Save option", "Save was clicked", "Button 2", "Button 1");
+        }
+
+    }
+}
